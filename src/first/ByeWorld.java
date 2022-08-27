f93c96df9e7d9a0cf80fa9de954fecda4c86e617
Ilknur <ilknur_yldz@hotmail.com>
2022-08-26 21:19:21 -0700

    added ByeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,10 @@
+package first;
+
+import javax.sound.midi.Soundbank;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Maybe 5 weeks of java left");
+        System.out.println("!.5 weeks of git");
+    }
+}
